17600a4b3133826a665f3b8c05026b3e63b5b45c
ketlasdev <hsn.ketlas@gmail.com>
2022-10-31 00:51:19 +0100

    Optimizing..

--- billing-service/src/main/java/me/ketlas/openlab/billingservice/exceptions/CustomerNotFoundException.java
+++ billing-service/src/main/java/me/ketlas/openlab/billingservice/exceptions/CustomerNotFoundException.java
@@ -1,7 +1,6 @@
 package me.ketlas.openlab.billingservice.exceptions;
 
 public class CustomerNotFoundException extends RuntimeException{
-
     public CustomerNotFoundException(String id){
         super(String.format("Customer with \"%s\" not found!",id));
     }
--- billing-service/src/main/java/me/ketlas/openlab/billingservice/repositories/InvoiceRepository.java
+++ billing-service/src/main/java/me/ketlas/openlab/billingservice/repositories/InvoiceRepository.java
@@ -7,6 +7,5 @@ import java.util.List;
 
 
 public interface InvoiceRepository extends JpaRepository<Invoice,String> {
-
     List<Invoice> findByCustomerID(String customerId);
 }
